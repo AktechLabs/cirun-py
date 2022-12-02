9603504ef95f0d7f2d81c076aa85272219af5e3c
Amit Kumar <dtu.amit@gmail.com>
2022-12-02 19:05:35 +0000

    Add command to get version

--- cirun/__about__.py
+++ cirun/__about__.py
@@ -1,4 +1,4 @@
 # SPDX-FileCopyrightText: 2022-present Amit Kumar <dtu.amit@gmail.com>
 #
 # SPDX-License-Identifier: MIT
-__version__ = '0.22'
+__version__ = '0.23'
--- cirun/main.py
+++ cirun/main.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import typer
 
 from cirun.cloud import cloud_app
@@ -7,8 +9,26 @@ app = typer.Typer(
     add_completion=False,
     no_args_is_help=True,
     rich_markup_mode="rich",
+    help="Cirun CLI 🚀",
     context_settings={"help_option_names": ["-h", "--help"]},
 )
+
+
+@app.callback(invoke_without_command=True)
+def version(
+        version_: Optional[bool] = typer.Option(
+            None,
+            "-v",
+            "--version",
+            help="Shows Cirun CLI version",
+            is_eager=True,
+        ),
+):
+    from .__about__ import __version__
+    if version_:
+        print(__version__)
+        raise typer.Exit()
+
 
 app.add_typer(repo_app, name="repo")
 app.add_typer(cloud_app, name="cloud")
