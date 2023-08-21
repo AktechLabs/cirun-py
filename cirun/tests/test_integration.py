5ad961ba20cacf9e951c0a0af0fbd7695bd10dc8
Amit Kumar <dtu.amit@gmail.com>
2023-08-22 00:29:11 +0530

    environment set test

--- cirun/tests/test_integration.py
+++ cirun/tests/test_integration.py
@@ -1,22 +1,31 @@
+import os
+
+import pytest
+
 from cirun import Cirun
 
 MSG_401 = "This API Key is expired or non-existent."
 
 
-def test_access_control_add_repo_to_resource_401():
+@pytest.fixture
+def set_api_key():
+    os.environ['CIRUN_API_KEY'] = "random-key"
+
+
+def test_access_control_add_repo_to_resource_401(set_api_key):
     cirun = Cirun()
     response = cirun.add_repo_to_resources("cirun", "cirun-py", resources=["cirun-runner-1"])
     assert response.status_code == 401
     assert response.json() == {"message": MSG_401}
 
 
-def test_access_control_remove_repo_from_resource_401():
+def test_access_control_remove_repo_from_resource_401(set_api_key):
     cirun = Cirun()
     response = cirun.add_repo_to_resources("cirun", "cirun-py", resources=["cirun-runner-1"])
     assert response.status_code == 401
     assert response.json() == {"message": MSG_401}
 
 
-def test_get_repo_resources():
+def test_get_repo_resources(set_api_key):
     cirun = Cirun()
     cirun.get_repo_resources(org="aktechlabs", repo="cirun-py")
