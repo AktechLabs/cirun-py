022009bff9b7b91eb9d3857cc0db7aba6fc586c7
Amit Kumar <dtu.amit@gmail.com>
2023-08-22 00:23:25 +0530

    fix 401 tests

--- cirun/tests/test_integration.py
+++ cirun/tests/test_integration.py
@@ -1,21 +1,20 @@
 from cirun import Cirun
 
-MSG_400 = ("User doesn't have admin permissions on the "
-           "access repository or Cirun application is not installed.")
+MSG_401 = "This API Key is expired or non-existent."
 
 
-def test_access_control_add_repo_to_resource_400s():
+def test_access_control_add_repo_to_resource_401():
     cirun = Cirun()
     response = cirun.add_repo_to_resources("cirun", "cirun-py", resources=["cirun-runner-1"])
-    response.status_code = 400
-    assert response.json() == {"message": MSG_400}
+    assert response.status_code == 401
+    assert response.json() == {"message": MSG_401}
 
 
-def test_access_control_remove_repo_from_resource_400():
+def test_access_control_remove_repo_from_resource_401():
     cirun = Cirun()
     response = cirun.add_repo_to_resources("cirun", "cirun-py", resources=["cirun-runner-1"])
-    response.status_code = 400
-    assert response.json() == {"message": MSG_400}
+    assert response.status_code == 401
+    assert response.json() == {"message": MSG_401}
 
 
 def test_get_repo_resources():
